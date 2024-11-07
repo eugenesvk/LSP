27cbda9ae904a74fa8b28058a2eb8ae7dca3526a
eugenesvk <eugenesvk@users.noreply.github.com>
2024-11-07 18:53:55 +0700

    add missing window arg

--- plugin/code_actions.py
+++ plugin/code_actions.py
@@ -441,7 +441,7 @@ class LspCodeActionsCommand(LspTextCommand):
     def _handle_response_async(self, session_name: str, response: Any) -> None:
         if isinstance(response, Error):
             message = f"{session_name}: {str(response)}"
-            notify_error(message, message)
+            notify_error(self.view.window(), message, message)
 
 
 # This command must be a WindowCommand in order to reliably hide corresponding menu entries when no view has focus.
@@ -504,7 +504,7 @@ class LspMenuActionCommand(LspWindowCommand, ABC):
     def _handle_response_async(self, session_name: str, response: Any) -> None:
         if isinstance(response, Error):
             message = f"{session_name}: {str(response)}"
-            notify_error(message, message)
+            notify_error(self.window, message, message)
 
     def _is_cache_valid(self, event: dict | None) -> bool:
         view = self.view
