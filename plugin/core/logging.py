a642494e07a7d93c79a530d47e0885c6a2310724
eugenesvk <eugenesvk@users.noreply.github.com>
2026-08-01 20:25:27 +0300

    accept optional sublime Window in notify functions
    fallback sublime functions

--- plugin/core/logging.py
+++ plugin/core/logging.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from .constants import ST_PACKAGES_PATH
-from typing import Any
+from typing import Any, Optional
 import inspect
 import threading
 import traceback
@@ -57,25 +57,33 @@ def printf(*args: Any, prefix: str = 'LSP') -> None:
     print(prefix + ":", *args)
 
 
-def notify(window: sublime.Window, message: str, status: str = 'LSP: see console log…') -> None:
-    """Pick either of the 2 ways to show a message:
-      - via a blocking modal dialog
-      - via a detailed console message and a short status message"""
+def notify(window: Optional[sublime.Window], message: str,
+           status: str = 'LSP: see console log…') -> None:
+    """Pick either of the 2 ways to show a user notification message:
+      - via a detailed console message and a short status message
+      - via a blocking modal dialog"""
     from .settings import userprefs
     if userprefs().suppress_error_dialogs:
-        window.status_message(status)
-        print(message)
+        if window:
+            window.status_message(status) # print short message to statusbar
+        else:
+            sublime.status_message(status)
+        print(message) # print full message to console log
     else:
-        window.message_dialog(message)
+        sublime.message_dialog(message)
 
 
-def notify_error(window: sublime.Window, message: str, status: str = '❗LSP: see console log…') -> None:
-    """Pick either of the 2 ways to show a message:
-      - via a blocking modal dialog
-      - via a detailed console message and a short status message"""
+def notify_error(window: Optional[sublime.Window], message: str,
+                 status: str = '❗LSP: see console log…') -> None:
+    """Pick either of the 2 ways to show a user error notification message:
+      - via a detailed console message and a short status message
+      - via a blocking error modal dialog"""
     from .settings import userprefs
     if userprefs().suppress_error_dialogs:
-        window.status_message(status)
-        print(message)
+        if window:
+            window.status_message(status) # print short message to statusbar
+        else:
+            sublime.status_message(status)
+        print(message) # print full message to console log
     else:
         sublime.error_message(message)
